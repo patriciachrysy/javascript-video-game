bdbd9c87ee3f5e7c7a570dbb468e7ad25d447cd1
Manezeu Patricia <pmanezeu@gmail.com>
2020-10-04 20:03:14 +0100

    Added all necessary assets

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,10 +7,13 @@ export default class GameScene extends Phaser.Scene {
 
   preload () {
     // load images
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('background', 'assets/background.png');
+    this.load.spritesheet('ground', 'assets/forest-2.png', { frameWidth: 132, frameHeight: 148 });
   }
 
   create () {
-    this.add.image(400, 300, 'logo');
+    this.add.image(400, 300, 'background');
+    let ground = this.physics.add.staticGroup();
+    ground.create(400, 568, 'ground', {start: 3, end: 3}).setScale(2).refreshBody();
   }
 };
